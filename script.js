8fc7414dc13982d94ded09643758926bcd84acd2
Katsuya <noryloth@gmail.com>
2025-06-02 17:37:30 +0800

    Add a function that creates grids Add a hover effect for each grid

---
+++ script.js
@@ -0,0 +1,36 @@
+const sizeOfGrid = 16;
+const container = document.querySelector(".container");
+
+const createGrid = (amtOfGrids) => {
+    
+    // Create a row 
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement('div'); 
+        row.classList.add('grid-row');
+
+        // and on every row create multiple boxes
+        for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / sizeOfGrid;
+            const box = document.createElement('div');
+            box.classList.add('box');
+
+            // Dynamic width & height
+            box.style.width = `${widthAndHeight}px`;
+            box.style.height = `${widthAndHeight}px`;
+
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
+}
+
+createGrid(sizeOfGrid);
+
+// Hover effect
+const allDivs = document.querySelectorAll(".box");
+allDivs.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'rgb(124, 111, 95)';
+    })
+})
+
